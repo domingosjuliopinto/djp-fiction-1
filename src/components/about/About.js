ff897c1d981b3037594348a6571c597e06c4742e
Domingos Julio Pinto <domingosjuliopinto@gmail.com>
2023-10-22 13:21:48 +0530

    Store Version 0.2.2

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,7 +3,34 @@ import './About.css';
 function About() {
   return (
     <div>
-    <h1>About</h1>
+      <h1>About</h1>
+        <br />
+        <h3>Game Information</h3>
+        <p></p>
+        <br />
+        <h3>About the Game</h3>
+        <h5>
+          <p>This game/story is completely free. This game/story site was created so that i could learn new stuff in web development and merge with my past hobbies.</p>
+          <p>The game/story storyline has fictitious plot and incidents. The characters and their names are fictitious too. In case (which will happen very rarely), if any character’s name is related to anyone’s real name in real life, it’s just a co-incident. Dialogues in the game/story, do not intent to hurt the sentiments of any real-life individuals.</p>
+        </h5> <h6>I HAVE A HUMBLE REQUEST: If you plan to use any of our characters' names, please do give credits. Even do send me your creation, I would like to read/see how my character is fits/used</h6>
+        <br />
+        <h3>Objectives for the creation of this site</h3>
+          <h5>To learn React.js</h5>
+          <h5>To learn the use of AI tools</h5>
+          <h5>To develop a web-game using my characters</h5>
+          <h5>To post a storyline using my characters</h5>
+        <br />
+        <h3>Sites used</h3>
+          <h5>Ai to create background (game locations) : <a href='https://www.fotor.com/features/ai-image-generator/'>https://www.fotor.com/features/ai-image-generator/</a></h5>
+          <h5>Change image to 16x9 ratio : <a href='https://imagy.app/change-image-to-16x9-aspect-ratio/'>https://imagy.app/change-image-to-16x9-aspect-ratio/</a></h5>
+          <h5>Convert picture to .eps file : <a href='https://vectorizer.ai/'>https://vectorizer.ai/</a></h5>
+          <h5>Image Compressor : <a href='https://imagecompressor.com/'>https://imagecompressor.com/</a></h5>
+          <h5>To Animate the drawings : <a href='https://sketch.metademolab.com/canvas'>https://sketch.metademolab.com/canvas</a></h5>
+          <h5>To Remove BG in Video and Convert to GIF : <a href='https://www.unscreen.com/upload'>https://www.unscreen.com/upload</a></h5>
+          <h5>Edit the GIF : <a href='https://ezgif.com/'>https://ezgif.com/</a></h5>
+        <br />
+        <h3>Contact</h3>
+        <h5>If any views , grievances (which I hope you don’t have) etc. You can email us on <a href="mailto:domingosjuliopinto@gmail.com">domingosjuliopinto@gmail.com</a></h5>
     </div>
   );
 }
