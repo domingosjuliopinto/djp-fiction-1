d912fceb6623ed706b2d83227a7540105edafa86
Domingos Julio Pinto <domingosjuliopinto@gmail.com>
2023-12-03 11:56:04 +0530

    Store Version 0.2.3

--- src/components/readstory/Readstory.js
+++ src/components/readstory/Readstory.js
@@ -1,9 +1,19 @@
 import './Readstory.css';
 
+import Title from '../readstory/chapters/Title.txt';
+
 function Readstory() {
+  fetch(Title)
+ .then(r => r.text())
+ .then(text => {
+  document.getElementById("title").innerText = text;
+  console.log(text);
+  });
+  
   return (
     <div>
     <h1>Read Story</h1>
+    <h1 id='title'> </h1>
     </div>
   );
 }
