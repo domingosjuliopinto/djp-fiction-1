2c51c5c8eab37b8b2dfd52e424fd33a634149830
Domingos Julio Pinto <domingosjuliopinto@gmail.com>
2024-04-14 18:52:49 +0530

    Store Version 0.1.1

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,54 @@
 import logo from '../../logo.svg';
 import './Home.css';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
 function Home() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className='App-header Vertical-align'>
+        <Container>
+          <Row>
+            <Col> <img src={logo} className="App-logo" alt="logo" /></Col>
+          </Row>
+          <br></br>
+          <Row>
+            <Col>
+              <div className="mb-2 d-grid gap-2">
+                <Button variant="outline-primary" size="lg">
+                  Read Story
+                </Button>
+              </div>
+            </Col>
+            <Col>
+              <div className="mb-2 d-grid gap-2">
+                <Button variant="outline-primary" size="lg">
+                  Characters
+                </Button>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+          <Col>
+              <div className="mb-2 d-grid gap-2">
+                <Button variant="outline-primary" size="lg">
+                  Extras
+                </Button>
+              </div>
+            </Col>
+            <Col>
+              <div className="mb-2 d-grid gap-2">
+                <Button variant="outline-primary" size="lg">
+                  About
+                </Button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
